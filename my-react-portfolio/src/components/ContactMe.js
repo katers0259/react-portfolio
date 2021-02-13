a6d14d8e709de80ef690cc5ef23df3a81e84144f
Kaitlyn Branagan <kaitlynwebb_46@hotmail.com>
2021-02-13 16:27:14 -0700

    adding resume and finishing contact page

--- my-react-portfolio/src/components/ContactMe.js
+++ my-react-portfolio/src/components/ContactMe.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import './Cards.css';
+
 import ContactMeCard from './ContactMeCard';
 import './ContactMe.css';
 
@@ -20,22 +20,27 @@ function ContactMe() {
                                         801-800-3411
                                     </div>
                                     <div className="cards">
-                                   <a href="https://www.facebook.com/katers0259" target="_blank"> <i class="fab fa-facebook"></i></a>
-                                    
-                                    <a href="https://www.instagram.com/ohbranagans/" target="_blank"><i class="fab fa-instagram"></i></a>
+                                        <a href="https://www.facebook.com/katers0259" target="_blank"> <i class="fab fa-facebook"></i></a>
 
-                                    <a href="https://codepen.io/katers0259" target="_blank">
-                                <i class="fab fa-codepen"></i>
-                            </a>
+                                        <a href="https://www.instagram.com/ohbranagans/" target="_blank"><i class="fab fa-instagram"></i></a>
 
-                            <a href="https://www.linkedin.com/in/kaitlyn-branagan-a119891b1/" target="_blank" >
-                                <i class="fab fa-linkedin-in"></i>
-                            </a>
+                                        <a href="https://codepen.io/katers0259" target="_blank">
+                                            <i class="fab fa-codepen"></i>
+                                        </a>
 
-                            <a href="https://github.com/katers0259" target="_blank"  >
-                                <i class="fab fa-github"></i>
-                            </a>
-                                    </div>
+                                        <a href="https://www.linkedin.com/in/kaitlyn-branagan-a119891b1/" target="_blank" >
+                                            <i class="fab fa-linkedin-in"></i>
+                                        </a>
+
+                                        <a href="https://github.com/katers0259" target="_blank"  >
+                                            <i class="fab fa-github"></i>
+                                        </a>
+                                        </div>
+                                        <div className="cards resume">
+                                        <a href="https://drive.google.com/file/d/1QcubR8-_WiX1CtYaMo0wdqjN9CIfRF7w/view?usp=sharing" download >
+                                        <i class="fas fa-file"></i>Download My Resume Here
+                                        </a>
+                                        </div>
                                 </ContactMeCard>
 
 
@@ -44,8 +49,8 @@ function ContactMe() {
                     </div>
                 </div>
             </div>
-       
-    </>
+
+        </>
     )
 
 }
