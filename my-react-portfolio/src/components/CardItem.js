7b03b3817a9b39de532233abbb785a9fbb621c33
Kaitlyn Branagan <kaitlynwebb_46@hotmail.com>
2021-02-14 15:09:46 -0700

    cant get a live link or repository link to work

--- my-react-portfolio/src/components/CardItem.js
+++ my-react-portfolio/src/components/CardItem.js
@@ -1,27 +1,36 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+
 import './Cards.css';
 
 
+
 function CardItem(props) {
-
-    
     return (
-        <>
-            <li className="cards_item">
-                <Link className="cards__item__link" to={props.path}>
-                    <figure className="cards__item__pic-wrap" data-category={props.label}>
-                        <img src={props.src} alt="project image" className="cards__item__img"></img>
-                    </figure>
-                    <div className="cards__item__info">
-                        <h5 className="cards__item__text">{props.text}</h5>
-                        <h5 className="repo">{props.children}</h5>
+    
+               
+                        <li className="cards__item">
+                            <Link className="cards__item__link">
 
-                    </div>
-                </Link>
-            </li>
-        </>
+                                <div className="cards__item__pic-wrap">
+                                    <img src={props.src} alt="" className="cards__item__img" />
+                                    
+                                    </div>
+                                    <div className="cards">{props.label}</div>
+                                    <h5 className="cards__item__text"></h5>
+                                    <div className="cards__item__info">
+                                    {props.text}
+                                    <a href={props.path} classname="link">Repo</a>
+                                    
+                                </div>
+                            </Link>
+                            
+                        </li>
+              
+        
+           
+        
+   
     )
 }
-
 export default CardItem
