e207df47bf09d3e0a1f373aaa21c155dd55e42fa
Kaitlyn Branagan <kaitlynwebb_46@hotmail.com>
2021-02-15 20:33:42 -0700

    fixing the cards

--- my-react-portfolio/src/components/Cards.js
+++ my-react-portfolio/src/components/Cards.js
@@ -1,89 +1,39 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
-import CardItem from './CardItem'
-import './Cards.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+ import './Cards.css';
+import Card from 'react-bootstrap/Card';
 
 
-function Cards() {
+function Cards(props) {
 
 
 
     return (
-        <div className="cards">
-            <h1>See My Work</h1>
-            <div className="cards__container">
-                <div className="cards__wrapper">
-                    <ul className="cards__items">
-                        <CardItem 
-                        src="https://res.cloudinary.com/dqtnonoqg/image/upload/v1610427708/my_team_jjzhgb.png"
-                        text="This is an App that allows you to see a list of employees and their jobs and titles, you can add, delete and update the list
-                        as you please.
-                        Languages used: Node.js, Javascript, HTML, CSS"
-                        label="Employee Tracker"
-                        path="https://github.com/katers0259/WhoIsOnYourTeam"
-                        
-                        >
+        <div className="cards__container">
+            <div className="cards__wrapper">
+                <div className="cards__items">
+        <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src={props.img}  />
+  <Card.Body>
+    <Card.Title>{props.title}</Card.Title>
+    <Card.Text>
+    {props.des}
+    </Card.Text>
+    <Card.Link href={props.liveLink} target="_blank">Live Link</Card.Link>
+    <Card.Link href={props.repo} target="_blank">Repo</Card.Link>
+  </Card.Body>
+</Card>
+</div>
+</div>
+</div>
 
-                        </CardItem>
-                                
-             
-                           
-                        <CardItem 
-                        src="https://res.cloudinary.com/dqtnonoqg/image/upload/v1610427708/passgenerator_smhmmz.png"
-                        text="This is a random password generator app, that can help you create passwords based on what you select for your
-                        preferences, i.e length, special characters, etc.
-                        Languages used: HTML, CSS, Javascript"
-                        label="Random Password Generator"
-                        path="https://github.com/katers0259/passgen"
-                        
-                        >
-                        
-                      </CardItem>
 
-                        <CardItem 
-                        src="https://res.cloudinary.com/dqtnonoqg/image/upload/v1610427708/macrame2_arp2gh.png"
-                        text="Macrame Madness, is a site that you can upload photos and designs of macrame to share with other enthusiats, you are presented with a 
-                        login page, that you must enter in you information before you are allowed into the site. You also have your own profile that you can 
-                        create, and update.
-                        Laguages used: HTML, TailwindCSS, JavaScript, MySQL, "
-                        label="Random Password Generator"
-                        path="https://github.com/katers0259/The-Best-Around-Project"
-                        ></CardItem>
-                        
-                        </ul>
-                        <ul className="cards__items">
-                        <CardItem 
-                        src="https://res.cloudinary.com/dqtnonoqg/image/upload/v1610427708/weather_dashboard_ezpjnq.png"
-                        text="This weather app allows the user to input any city, and see the current weather, and also the 5 day forecast.
-                        Laguages used: HTML, CSS, Javascript, API "
-                        label="Weather Dashboard"
-                        path="https://github.com/katers0259/BranaganWeatherDashboard"
-                        ></CardItem>
 
-                       
-                          <CardItem 
-                        src="https://res.cloudinary.com/dqtnonoqg/image/upload/v1610427709/search_cl9smp.png"
-                        text="Pick your Pet, uses API calls to a petfinder API, that allows you to search for dogs or cats with specifications that 
-                        you select and then displays those animals that are available for adoption.
-                        Laguages used: HTML, CSS, Javascript, API "
-                        label="Pick Your Pet"
-                        path="https://github.com/katers0259/Pick-your-pet"
-                        path1=""
-                        />
-                             <CardItem 
-                        src="https://res.cloudinary.com/dqtnonoqg/image/upload/v1613202799/workout_jvm7ph.png"
-                        text="This workout fitness tracker lets the user input workouts, and tracks the specifications on a graph and displays this to the user
-                        on their personal dashboard.
-                        Laguages used: HTML, CSS, Javascript, MongoDB Atlas
-                        This App is deployed on Heroku. "
-                        label="Fitness Tracker"
-                        path="https://github.com/katers0259/Fitness-Whole-App"
-                        
-                        />
-                    </ul>
-                </div>
-            </div>
-        </div>
+
+
+
+
     )
 }
 
--- my-react-portfolio/src/components/pages/Projects.js
+++ my-react-portfolio/src/components/pages/Projects.js
@@ -1,10 +1,35 @@
 import React from 'react'
 import Cards from '../Cards';
+import projects from '../projects.json'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 function Projects() {
     return (
         <div>
-            <Cards />
+            <h1>See My Work</h1>
+            {projects.map(result => {
+                return(
+                    
+                    <Container>
+                        <Row className="justify-content-center">
+                            <Col lg="auto">
+                    <Cards 
+                    key={result.id}
+                    title={result.title}
+                    img={result.img}
+                    des={result.des}
+                    repo={result.repo}
+                    liveLink={result.liveLink}
+                    />
+                    </Col>
+                    </Row>
+                    </Container>
+                )
+            })}
+            
         </div>
     )
 }
