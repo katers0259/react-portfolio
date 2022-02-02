ed6b35f15aa3fd5d8eb9791a7d2b59a0ac617f8e
kaitlyn brangan <87716201+KateHydroJug@users.noreply.github.com>
2022-02-02 15:10:41 -0700

    making more updates to portfolio

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -16,7 +16,7 @@ export default function AboutMe() {
                         src="https://res.cloudinary.com/dqtnonoqg/image/upload/v1610427708/portfolio-photo1_rfmuwo.jpg"
                     >
                         <div className="cards">
-                            <h1>About Myself</h1>
+                            <h1 class="skills">About Myself</h1>
                             </div>
                        <p class="about-me"> My Name is Kaitlyn Branagan. I have a love for full stack web development. I am excellent
                         at creating and maintaining great client relationships. I have 10 plus years of customer
@@ -24,7 +24,7 @@ export default function AboutMe() {
                         to manage a team of employees, while meeting goals.</p>
                                 <br></br>
                         <div className="cards">
-                            <h1>Languages</h1>
+                            <h1 class="skills">Languages</h1>
                         </div>
                         <div className="cards list-display">
                             
@@ -32,18 +32,31 @@ export default function AboutMe() {
                                 <li class="list-dot-remove"><i class="fab fa-css3-alt"> CSS</i></li>
                                 <li class="list-dot-remove"><i class="fab"> Shopify Liquid </i></li>
                                 <li class="list-dot-remove"><i class="fab fa-node-js"> Node JS</i></li>
-                                <li class="list-dot-remove"><i class="fas fa-database"> My SQL, MongoDB</i></li>
+                                <li class="list-dot-remove"><i class="fas fa-database"> My SQL </i></li>
+                                <li class="list-dot-remove"> MongoDB</li>
                                 <li class="list-dot-remove"><i class="fab fa-react"> React</i></li>
                                 <li class="list-dot-remove"> Handlebars.js</li>
                                 <li class="list-dot-remove"> Express.js</li>
-                                <li class="list-dot-remove"><i class="fab fa-js"> Javascript, JQuery</i></li>
+                                <li class="list-dot-remove"><i class="fab fa-js"> Javascript</i></li>
+                                <li class="list-dot-remove"> JQuery</li>
                                 <li class="list-dot-remove">Tailwind CSS</li>
                         </div>
                         <div className="cards">
-                            <h2>Skills</h2>
+                            <h2 class="skills">Skills</h2>
                             </div>
-                            <p>Team Management, Problem Solving, Debugging, Customer Service, Client Relations,
-                    Training, Time Management, Team Oriented, Self Motivated.</p>
+                            <p >
+                              
+                                <li class="list-dot-remove" >Team Management</li> 
+       
+                                    <li class="list-dot-remove">Problem Solving</li>
+                                    <li class="list-dot-remove">Debugging</li>
+                                    <li class="list-dot-remove">Customer Service</li>
+                                    <li class="list-dot-remove">Client Relations</li>
+                                    <li class="list-dot-remove">Self Motivated</li>
+                                    <li class="list-dot-remove">Team Oriented</li>
+                                    <li class="list-dot-remove">Time Management</li>
+                                    <li class="list-dot-remove">Training</li>
+                               </p>
 
                        
                     </AboutMeCard>
