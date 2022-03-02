cdcb5663ef51f55d857fa579288dca9533252d42
Kaitlyn Branagan <69743567+katers0259@users.noreply.github.com>
2022-03-02 14:25:25 -0700

    Update HeroSection.js

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -10,7 +10,7 @@ function HeroSection() {
         <div className='hero-container'>
             <video src="https://res.cloudinary.com/dqtnonoqg/video/upload/v1613200421/Pexels_Videos_1321208_xol8an.mp4" autoPlay loop muted />
             <h1>Kaitlyn Branagan</h1>
-            <p>Full Stack Web Developer</p>
+            <p>Please Visit  <a href="https://updatedportfoliokaitlynbran.herokuapp.com/"> My New Portfolio Here </a> </p>
             <div className="hero-btns">
              <Button  className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>
                     GitHub
